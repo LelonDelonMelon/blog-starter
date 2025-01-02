b6d41aed180c0ad476fd4ad7f56ed2be4c22bae0
LelonDelonMelon <burkinggs@gmail.com>
2025-01-02 19:50:56 +0300

    ready to use, file based routing, category filtering

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'My Blog';
+export const BLOG_PAGE_TITLE = 'Blog Posts';
+export const SITE_DESCRIPTION = 'Enjoy your reading!';
